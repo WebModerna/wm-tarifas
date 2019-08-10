549d6d742f931c509bc4dea2c434e2e503fe271f
WebModerna <edgardogalletto@gmail.com>
2019-08-10 13:46:46 -0300

    Activación del script

--- admin/configuracion.php
+++ admin/configuracion.php
@@ -20,33 +20,33 @@ Se debe poder editar los títulos como el contenido de las celdas. Todo será un
 	<header class="primary">
 		<h1><?php _e( 'Planilla de Tarifas y Temporadas', 'text_domain' ) ?></h1>
 	</header>
-	<form action="opciones.php" method="post">
-		<table class="wp-list-table widefat fixed striped media">
+	<form action="opciones.php" method="post" id="wm_tarifazos">
+		<table class="form-table striped">
 			<caption>
-				<h3><?php _e('Agregar, editar y borrar títulos de las columnas, filas y celdas.', 'text_domain');?></h3>
+				<h3><?php _e('Agregar aquí los nombres de las temporadas ↓', 'text_domain');?></h3>
 			</caption>
-			<thead>
+			<tbody>
+
 				<tr>
 					<th class="column-title">
-						<input type="text" name="" id="" placeholder="<?php _e('Habitaciones o Cabañas', 'text_domain');?>" />
+						<input class="room_title" disabled type="text" name="habit" id="habit" placeholder="<?php _e('Habitaciones o Cabañas', 'text_domain');?>" />
 					</th>
 					<th>
-						<input type="text" name="" id="" placeholder="<?php _e('Temporada Alta', 'text_domain');?>" />
+						<input class="room_title" disabled type="text" name="temp_1" id="temp_1" placeholder="<?php _e('Temporada Alta', 'text_domain');?>" />
 					</th>
 					<th>
-						<input type="text" name="" id="" placeholder="<?php _e('Temporada Media', 'text_domain');?>" />
+						<input class="room_title" disabled type="text" name="temp_2" id="temp_2" placeholder="<?php _e('Temporada Media', 'text_domain');?>" />
 					</th>
 					<th>
-						<input type="text" name="" id="" placeholder="<?php _e('Temporada Baja', 'text_domain');?>" />
+						<input class="room_title" disabled type="text" name="temp_3" id="temp_3" placeholder="<?php _e('Temporada Baja', 'text_domain');?>" />
 					</th>
 					<th>
 						<div class="botones_edicion">
-							<a href="#" class="boton_editar"><span class="dashicons-before dashicons-edit"></span></a>
+							<a href="#" id="boton_editar_titulo"><span class="dashicons-before dashicons-edit"></span></a>
 						</div>
 					</th>
 				</tr>
-			</thead>
-			<tbody>
+
 				<tr class="tarifa_fila">
 					<td>
 						<input type="text" min="0" value="" placeholder="Habitación xxx" name="titulo1" id="titulo1" disabled="disabled" />
@@ -89,8 +89,8 @@ Se debe poder editar los títulos como el contenido de las celdas. Todo será un
 
 		<div>
 			<hr />
-			<input type="submit" class="button-primary" name="update" value="<?php esc_attr_e( 'Guardar Opciones', 'text_domain' ); ?>" />
-			<input type="submit" class="reset-button button-secondary" name="reset" value="<?php esc_attr_e( 'Restaurar valores por defecto', 'text_domain' ); ?>" onclick="return confirm( '<?php print esc_js( __( 'Click OK para resetear. Cualquier cambio hecho se perderá!', 'text_domain' ) ); ?>' );" />
+			<input type="submit" name="enviar"  value="<?php _e("Guardar Opciones", "text-domain");?>" class="button-primary" id="submit" />
+			<input type="reset" class="reset-button button-secondary" name="reset" value="<?php esc_attr_e( 'Restaurar valores por defecto', 'text_domain' ); ?>" onclick="return confirm( '<?php print esc_js( __( 'Click OK para resetear. Cualquier cambio hecho se perderá!', 'text_domain' ) ); ?>' );" />
 			<div class="clear"></div>
 		</div>
 	</form>
--- admin/js/wm_admin.js
+++ admin/js/wm_admin.js
@@ -1,2 +1,30 @@
 // Mis scripts administrativos
-console.log('probando la cargad de este plugin');
+(function()
+{
+	jQuery(document).ready(function($){
+		//you can now use $ as your jQuery object.
+		var body = $( 'body' );
+		
+		// dentro de esta función $() funcionará como un álias de jQuery()
+		$(document).on("ready", wm_edicion);
+
+		function wm_edicion()
+		{
+			$("#boton_editar_titulo").on("click", puchito);
+
+			function puchito()
+			{
+				if( $(".room_title").attr("disabled", true) )
+				{
+					$(".room_title").attr("disabled", false);
+					console.log("lo habilite");
+				}
+				else
+				{
+					console.log("lo deshabilite");
+					$(".room_title").attr("disabled", true);
+				}
+			};
+		}
+	});
+}())
